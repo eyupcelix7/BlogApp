dc2df2ab34a3e87b537bcfde998b403d6258b3da
Eyüp Çelik <eyupturkey641@gmail.com>
2025-11-26 04:04:25 +0300

    Sqlite Veritabanına Geri Dönüldü ve Posts Repository Oluşturuldu

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
+using BlogApp.Data.Abstract;
 using BlogApp.Data.Concrete.EfCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BlogContext>(options =>
 {
     var config = builder.Configuration;
-    var connectionString = config.GetConnectionString("mysql_connection");
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8,0,44)));
+    var connectionString = config.GetConnectionString("sqlite_connection");
+    //options.UseMySql(connectionString, new MySqlServerVersion(new Version(8,0,44)));
+    options.UseSqlite(connectionString);
 });
+builder.Services.AddScoped<IPostRepository, EfPostRepository>();
 var app = builder.Build();
 
 SeedData.TestVerileriniDoldur(app);
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 
 app.Run();
