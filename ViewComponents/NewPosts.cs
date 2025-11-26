27aad4e3083c4612535f6e5557fb976caa23a3d3
Eyüp Çelik <eyupturkey641@gmail.com>
2025-11-26 17:16:34 +0300

    NewPosts Componenti Oluşturuldu Componentler asenkron yapıya dönüştürüldü

---
+++ ViewComponents/NewPosts.cs
@@ -0,0 +1,26 @@
+﻿using BlogApp.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.ViewComponents
+{
+    public class NewPosts: ViewComponent
+    {
+        IPostRepository _postRepository { get; set; }
+
+        public NewPosts(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(
+                await _postRepository
+                .Posts
+                .OrderByDescending(p=> p.PublishedOn)
+                .Take(10)
+                .ToListAsync()
+            );
+        }
+    }
+}
